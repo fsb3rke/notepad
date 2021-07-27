89fd317d410546e4f600a14273ca981a0e1a2b97
Berkee <berkeeavcii@gmail.com>
2021-07-27 11:29:01 +0300

    Merhaba arkadaslar

--- src/main.java
+++ src/main.java
@@ -2,19 +2,6 @@ import javax.swing.*;
 
 public class main {
     public static void main(String[] args) {
-
-        try {
-            UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
-        } catch (ClassNotFoundException e) {
-            e.printStackTrace();
-        } catch (InstantiationException e) {
-            e.printStackTrace();
-        } catch (IllegalAccessException e) {
-            e.printStackTrace();
-        } catch (UnsupportedLookAndFeelException e) {
-            e.printStackTrace();
-        }
-
         SwingUtilities.invokeLater(new Runnable() {
             @Override
             public void run() {
